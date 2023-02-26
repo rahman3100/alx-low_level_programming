f62114ccc6a85772f6284245e2aa7806d0280755
rahman3100 <kolawole3100@gmail.com>
2023-02-26 02:55:06 -0800

    B

---
+++ 0x02-functions_nested_loops/2-main.c
@@ -0,0 +1,13 @@
+julien@ubuntu:~/0x02$ cat 2-main.c
+#include "main.h"
+
+/**
+ * main - check the code.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    print_alphabet_x10();
+    return (0);
+}
